0815962b8806c024183d1d0276dbc738a314ed85
AndreWar10 <andregs.dev@gmail.com>
2025-09-27 02:49:34 -0300

    feat -> add get_it (dependency_injection)

---
+++ safe_qr_app/lib/injection.dart
@@ -0,0 +1,56 @@
+import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'core/theme/data/repositories/theme_repository_impl.dart';
+import 'core/theme/domain/repositories/theme_repository.dart';
+import 'core/theme/domain/usecases/get_theme.dart';
+import 'core/theme/domain/usecases/save_theme.dart';
+import 'core/theme/presentation/bloc/theme_bloc.dart';
+import 'core/logger/data/repositories/logger_repository_impl.dart';
+import 'core/logger/domain/repositories/logger_repository.dart';
+import 'core/logger/domain/usecases/log_message.dart';
+import 'core/logger/domain/services/logger_service.dart';
+
+/// Service Locator global da aplicação
+final getIt = GetIt.instance;
+
+/// Configura todas as dependências da aplicação
+Future<void> configureDependencies() async {
+  // External dependencies
+  final prefs = await SharedPreferences.getInstance();
+  getIt.registerSingleton<SharedPreferences>(prefs);
+
+  // Theme dependencies
+  getIt.registerLazySingleton<ThemeRepository>(
+    () => ThemeRepositoryImpl(getIt<SharedPreferences>()),
+  );
+
+  getIt.registerLazySingleton<GetTheme>(
+    () => GetTheme(getIt<ThemeRepository>()),
+  );
+
+  getIt.registerLazySingleton<SaveTheme>(
+    () => SaveTheme(getIt<ThemeRepository>()),
+  );
+
+  getIt.registerFactory<ThemeBloc>(
+    () => ThemeBloc(
+      getTheme: getIt<GetTheme>(),
+      saveTheme: getIt<SaveTheme>(),
+    ),
+  );
+
+  // Logger dependencies
+  getIt.registerLazySingleton<LoggerRepository>(
+    () => LoggerRepositoryImpl(),
+  );
+
+  getIt.registerLazySingleton<LogMessage>(
+    () => LogMessage(getIt<LoggerRepository>()),
+  );
+
+  getIt.registerLazySingleton<LoggerService>(
+    () => LoggerService(
+      logMessage: getIt<LogMessage>(),
+    ),
+  );
+}
--- safe_qr_app/lib/main.dart
+++ safe_qr_app/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:safe_qr_app/core/my_app.dart';
+import 'package:safe_qr_app/injection.dart' as injection;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await injection.configureDependencies();
   runApp(const MyApp());
 }
-
